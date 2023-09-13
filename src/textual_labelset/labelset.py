622eec2894c1d3c9db8740a16ea048ff683ea598
Steve Holden <steve@holdenweb.com>
2023-09-13 10:27:23 +0100

    Remove debug prints, add type info to signatures.

--- src/textual_labelset/labelset.py
+++ src/textual_labelset/labelset.py
@@ -4,6 +4,7 @@ from textual.widgets import Static, Rule
 from textual.containers import Vertical, Horizontal, VerticalScroll
 from rich.text import Text
 
+from typing import Callable
 
 def sort_key(x):
     k, v = x
@@ -12,10 +13,22 @@ def sort_key(x):
 
 
 class LabelSet(Static):
+    """A set of labels that render as a static.
 
-    DEFAULT_CSS = "LabelSet {width: 30; height: auto; margin: 2; border: yellow 100%; }"
+    Args:
+        members: An iterable of string values.
+        action_func: The action to be taken when a member's link is clicked.
+        fmt: The format of a member in the LabelSet's string representation.
+        key: The key function used to sort the members.
+    """
+    DEFAULT_CSS = "LabelSet { width: 30; height: auto; margin: 2; border: yellow 100%; }"
 
-    def __init__(self, members, action_func, fmt, key=None, *args, **kw):
+    def __init__(self,
+                 members: list[str],
+                 action_func: Callable[[int], None],
+                 fmt: str, key=None,
+                 *args,
+                 **kw):
         super().__init__(*args, **kw)
         self.action_func = action_func
         def local_key(x):
@@ -23,20 +36,26 @@ class LabelSet(Static):
             return list(seq)
         self.fmt = fmt
         self.key = local_key if key is None else key
-        print("MEMBERS:", members)
         self.members = dict(sorted(members.items(), key=self.key))
-        print("SORTED MEMBERS:", self.members)
 
     def render(self):
         strings = [self.fmt.format(i=i, v=v) for (i, v) in self.members.items()]
         return Text.from_markup(" ".join(strings))
 
-    def action_click(self, i):
+    def action_click(self, i: int):
         return self.action_func(i)
 
 
 class LabelSetSelector(Widget):
-    def __init__(self, selected, unselected, *args, **kw):
+    """Select a set of labels from a closed vocabulary.
+
+    Args:
+        selected: An iterable of the currently selected labels.
+        unselected: An iterable of the currently deselected labels.
+    """
+    DEFAULT_CSS = "LabelSetDelector { border: yellow 100%; }"
+
+    def __init__(self, selected: list[str], unselected: list[str], *args, **kw) -> None:
         super().__init__(*args, **kw)
         self.selected = dict(enumerate(selected))
         self.unselected = dict(enumerate(unselected, start=len(selected)))
@@ -46,21 +65,21 @@ class LabelSetSelector(Widget):
             yield self.selected_labels()
             yield self.unselected_labels()
 
-    def update_view(self):
+    def update_view(self) -> None:
         v = self.query_one("#lss-selector")
         v.remove_children()
         v.mount(self.selected_labels())
         v.mount(self.unselected_labels())
 
-    def unselected_labels(self):
+    def unselected_labels(self) -> LabelSet:
         return LabelSet(
             members=self.unselected,
             action_func=self.select,
-            fmt="\\[[@click='click({i})'][red]{v}[/red][/]]",
+            fmt="\\[[@click='click({i})']{v}[/]]",
             classes="label-set selected-labels",
         )
 
-    def selected_labels(self):
+    def selected_labels(self) -> LabelSet:
         return LabelSet(
             members=self.selected,
             action_func=self.deselect,
@@ -68,20 +87,22 @@ class LabelSetSelector(Widget):
             classes="label-set unselected-labels",
         )
 
-    def deselect(self, i):
+    def deselect(self, i: int) -> None:
         assert i in self.selected
         self.unselected[i] = self.selected[i]
         del self.selected[i]
         self.update_view()
 
-    def select(self, i):
+    def select(self, i: int) -> None:
         assert i in self.unselected
         self.selected[i] = self.unselected[i]
         del self.unselected[i]
         self.update_view()
 
 class WideLabelSetSelector(LabelSetSelector):
-
+    """
+    LabelSetSelector with a custom layout.
+    """
     def compose(self) -> ComposeResult:
         with VerticalScroll(id="lss-selector"):
             yield self.selected_labels()
@@ -91,7 +112,7 @@ class WideLabelSetSelector(LabelSetSelector):
 s = "Tom Dick Harry".split()
 u = "Charlie Joe Quentin".split()
 
-def build_app(s, u):
+def build_app(s: list[str], u: list[str]) -> App:
 
     class SelTestApp(App):
         def compose(self):
